2fa7e73469600e263b94880b3ffc82209f7db076
Dongju Lee <dannyldj@naver.com>
2020-11-04 18:48:42 +0900

    upload

---
+++ apitest/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using apitest.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+namespace apitest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static LoginViewModel LoginViewModel = new LoginViewModel();
+    }
+}
---
+++ apitest/ViewModel/LoginViewModel.cs
@@ -0,0 +1,34 @@
+﻿using apitest.manager;
+using apitest.Model;
+using Newtonsoft.Json.Linq;
+using Prism.Mvvm;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apitest.ViewModel
+{
+    public class LoginViewModel : BindableBase
+    {
+        RestManager restmanager = new RestManager();
+
+        private string _token;
+        public string Token
+        {
+            get => _token;
+            set => SetProperty(ref _token, value);
+        }
+
+        public async Task Login(string Id, string Password)
+        {
+            JObject jobj = new JObject();
+            jobj.Add("userId", Id);
+            jobj.Add("password", Password);
+            var loginResult = await restmanager.RestRequest<LoginModel>(jobj.ToString(), null, Method.POST, "/signIn");
+            Token = loginResult.token;
+        }
+    }
+}
