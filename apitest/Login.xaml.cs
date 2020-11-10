ece972a7e42bbcdcd716ded4aebe0d2392c9929d
Dongju Lee <dannyldj@naver.com>
2020-11-10 17:11:34 +0900

    design

---
+++ apitest/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using apitest.manager;
+using apitest.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace apitest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = App.LoginViewModel;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            onLogin();
+        }
+        private async void onLogin()
+        {
+           await App.LoginViewModel.Login(tbID.Text, tbPassword.Text);
+        }
+    }
+}
---
+++ apitest/Model/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apitest.Model
+{
+    class RegisterModel
+    {
+        public string result { get; set; }
+    }
+}
