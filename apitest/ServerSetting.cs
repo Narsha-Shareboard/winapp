b9cf157e24faa084185c4ca3097c18cddd331da7
Dongju Lee <dannyldj@naver.com>
2020-11-17 17:26:58 +0900

    signin design

---
+++ apitest/ServerSetting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apitest
+{
+    public class ServerSetting
+    {
+        public static string ServerUri = "http://10.80.163.75:8080";
+    }
+}
