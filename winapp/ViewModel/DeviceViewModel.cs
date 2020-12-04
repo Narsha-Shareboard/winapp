fe6619b55543b85fe3b3bb5bea0f50654ade2290
Dongju Lee <dannyldj@naver.com>
2020-12-04 10:05:37 +0900

    various fix

---
+++ winapp/ViewModel/DeviceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using winapp.manager;
+using winapp.Model;
+using winapp.WPage;
+using Newtonsoft.Json.Linq;
+using Prism.Mvvm;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace winapp.ViewModel
+{
+    public class DeviceViewModel
+    {
+
+    }
+}
---
+++ winapp/WPage/Push.xaml.cs
@@ -0,0 +1,48 @@
+﻿using winapp.manager;
+using winapp.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using winapp.Properties;
+
+namespace winapp.WPage
+{
+    /// <summary>
+    /// Interaction logic for Push.xaml
+    /// </summary>
+    public partial class Push : Page
+    {
+        private readonly MainWindow _mainWindow;
+
+        public Push(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+            Loaded += Push_Loaded;
+        }
+        public Push()
+        {
+            InitializeComponent();
+            Loaded += Push_Loaded;
+        }
+
+        private void Push_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = App.PushViewModel;
+        }
+    }
+}
